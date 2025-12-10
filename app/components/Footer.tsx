128a0e142f413c2fd0a3fc5ace0e1a1cc9f812c7
Applepiea <vapel.rosenberg@gmail.com>
2025-12-10 07:34:05 +0100

    implement Footer component with shopping information and links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+import { footerLinks } from '@/lib/constants'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='info'>
+      <p>
+        More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.
+        <img src="/logo.svg" alt="apple logo" />
+      </p>
+
+      <hr />
+
+      <div className="links">
+        <p>
+          Copyright © 2024 Apple Inc. All rights reserved.
+        </p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
